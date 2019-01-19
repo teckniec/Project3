31fd9fca0ad88f9ee420d3cadfd4c5d79ffebdc0
Anthony Miller <1anthony1@gmail.com>
2019-01-19 10:34:39 -0700

    new code

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import Print from "./pages/Print";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Login from './components/auth/Login';
 
+function onAuthRequired({ history }) {
+  history.push('/login');
+}
 function App() {
   return (
     <Router>
+      <Security
+          issuer="https://dev-254707.oktapreview.com/oauth2/default"
+          client_id="0oaio6v9b84q31sUA0h7"
+          redirect_uri={window.location.origin + '/implicit/callback'}
+          onAuthRequired={onAuthRequired}
+        >
       <div>
   
         <Switch>
-          <Route exact path="/" component={Books} />
+          <SecureRoute exact path="/" component={Books} />
+          <Route
+                path="/"
+                render={() => (
+                  <Login baseUrl="https://dev-254707.oktapreview.com" />
+                )}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
           <Route exact path="/books" component={Books} />
           {/* <Route exact path="/books/:id" component={Detail} /> */}
           <Route exact path="/menu" component={Detail} />
@@ -20,6 +38,7 @@ function App() {
           <Route component={NoMatch} />
         </Switch>
       </div>
+      </Security>
     </Router>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import { Link } from "react-router-dom";
+import { Input, TextArea, FormBtn} from "../components/Form"
