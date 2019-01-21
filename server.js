9f1bca0feaa85184839ffa11c6a67c5132dfc233
teckniec <1anthony1@gmail.com>
2019-01-20 19:25:26 -0700

    cors troubleshooting

--- server.js
+++ server.js
@@ -4,10 +4,39 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+app.options('*', cors());
+
+
+// CORS
+const whitelist = [
+  'https://dev-254707.oktapreview.com/oauth2/default', 
+  'https://dev-254707.oktapreview.com', 
+  'http://localhost:3000/implicit/callback',
+  'https://developer.okta.com'
+]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// Then pass them to cors:
+app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -22,3 +51,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglis
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
